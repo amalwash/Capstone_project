9a342d58c816ff8a5bea77454ad5d0d9913ed6de
amal wash <8amal.8ll@gmail.com>
2021-12-21 19:05:41 +0300

    Day3

---
+++ Day3/back end/src/main/java/com/example/carnextdoor/controller/CarController.java
@@ -0,0 +1,40 @@
+package com.example.carnextdoor.controller;
+import com.example.carnextdoor.model.Entitiec.Car;
+import com.example.carnextdoor.model.Service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping(path = "api/car")
+public class CarController {
+    private CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping
+    public List< Car > getCars() {
+        return carService.getCars();
+    }
+
+
+    @GetMapping(path = "{carId}")
+    public Optional< Car > getCar(@PathVariable(name = "carId") Integer carId) {
+        return carService.getCar(carId);
+    }
+
+    @PostMapping("add")
+    public void registerNewCar(@RequestBody Car car) {
+        carService.addNewCar(car);
+    }
+//@DeleteMapping(path = "delete/{carId}")
+//    public void deleteCar(@PathVariable("carId")Integer carId){
+//        carService.deleteCar(carId);
+//}
+
+}
---
+++ Day3/back end/src/main/java/com/example/carnextdoor/model/Entitiec/Owner.java
@@ -0,0 +1,99 @@
+package com.example.carnextdoor.model.Entitiec;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Owner {
+    @Id
+    private int owner_id;
+    private String owner_firstName;
+    private String owner_lastName;
+    private String owner_address;
+    private int owner_phone;
+    private int owner_age;
+    private String owner_email;
+
+    public Owner(int owner_id, String owner_firstName, String owner_lastName, String owner_address, int owner_phone, int owner_age, String owner_email) {
+        this.owner_id = owner_id;
+        this.owner_firstName = owner_firstName;
+        this.owner_lastName = owner_lastName;
+        this.owner_address = owner_address;
+        this.owner_phone = owner_phone;
+        this.owner_age = owner_age;
+        this.owner_email = owner_email;
+    }
+
+    public Owner() {
+    }
+
+    public int getOwner_id() {
+        return owner_id;
+    }
+
+    public void setOwner_id(int owner_id) {
+        this.owner_id = owner_id;
+    }
+
+    public String getOwner_firstName() {
+        return owner_firstName;
+    }
+
+    public void setOwner_firstName(String owner_firstName) {
+        this.owner_firstName = owner_firstName;
+    }
+
+    public String getOwner_lastName() {
+        return owner_lastName;
+    }
+
+    public void setOwner_lastName(String owner_lastName) {
+        this.owner_lastName = owner_lastName;
+    }
+
+    public String getOwner_address() {
+        return owner_address;
+    }
+
+    public void setOwner_address(String owner_address) {
+        this.owner_address = owner_address;
+    }
+
+    public int getOwner_phone() {
+        return owner_phone;
+    }
+
+    public void setOwner_phone(int owner_phone) {
+        this.owner_phone = owner_phone;
+    }
+
+    public int getOwner_age() {
+        return owner_age;
+    }
+
+    public void setOwner_age(int owner_age) {
+        this.owner_age = owner_age;
+    }
+
+    public String getOwner_email() {
+        return owner_email;
+    }
+
+    public void setOwner_email(String owner_email) {
+        this.owner_email = owner_email;
+    }
+
+    @Override
+    public String toString() {
+        return "Owner{" +
+                "owner_id=" + owner_id +
+                ", owner_firstName='" + owner_firstName + '\'' +
+                ", owner_lastName='" + owner_lastName + '\'' +
+                ", owner_address='" + owner_address + '\'' +
+                ", owner_phone=" + owner_phone +
+                ", owner_age=" + owner_age +
+                ", owner_email='" + owner_email + '\'' +
+                '}';
+    }
+}
